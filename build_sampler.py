06cb86cac72b6a3150bbef0a17ac540a78b464c9
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2016-07-20 22:34:27 -0400

    Remove printlines.

--- build_sampler.py
+++ build_sampler.py
@@ -8,8 +8,6 @@ from citibike_trips import DataStore
 db = DataStore()
 coords_list = []
 for geojson_repr in db.select_all():
-    print(geojson_repr)
-    print(geojson_repr.keys())
     for feature in geojson_repr['features']:
         coords_list.append(feature['geometry']['coordinates'])
 with open('bike_week_sampler.json', 'w') as f:
